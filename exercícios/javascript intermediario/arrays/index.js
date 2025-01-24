93f0f928b0ad54ca84828b104cc5667332213b67
Arthursant7s830 <135383243+Arthursant7s830@users.noreply.github.com>
2025-01-24 15:01:58 -0300

    js intermediario

---
+++ exercícios/javascript intermediario/arrays/index.js
@@ -0,0 +1,16 @@
+/*let filmes = ['Vingadores', 'Batman', 'Shrek']
+console.log(filmes[0])
+console.log(filmes[1])
+console.log(filmes[2])
+
+
+let descricaoFilme = ['Vingadores', 4.5, true]
+console.log(descricaoFilme)*/
+
+//ARRAYS MULTIDIMENSIONAIS
+
+let filmes = [
+    ['Vingadores', 4.5, true, ['Tony', 50]],
+    ['Shrek', 4.6, false]
+]
+console.table(filmes[0][3][0])
