c6edba2678787711e17e182c1fb2b86d822c4a88
Arthursant7s830 <135383243+Arthursant7s830@users.noreply.github.com>
2025-01-30 15:10:33 -0300

    up

---
+++ exercícios/javascript intermediario/erros-js/index.js
@@ -0,0 +1,11 @@
+/*let barBoleto = 100; 
+console.log(barBoleto)*/
+let inputTelefone = document.getElementById('telefone');
+
+console.log(inputTelefone)
+
+inputTelefone.disabled = true;
+
+function habilitarTelefone(){
+
+}
