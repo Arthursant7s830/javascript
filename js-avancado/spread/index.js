6885217402764288390d69f92b3d316f078ef4d6
Arthursant7s830 <135383243+Arthursant7s830@users.noreply.github.com>
2025-02-11 18:22:53 -0300

    js avançado

---
+++ js-avancado/spread/index.js
@@ -0,0 +1,16 @@
+// const pessoas = ['Arthur', 'lima', 20]
+// console.log(...pessoas)
+
+//CONCATENAR DOIS ARRAYS
+
+// let pessoas1 = ['Arthur', 'Santos', 'Lima']
+// let pessoas2 = ['Pedro', 'João', 'Paulo']
+
+// pessoas1 = [...pessoas1, ...pessoas2, ...pessoas3]
+// console.log(pessoas1)
+
+const pessoa1 = {nome: 'Arthur', idade: 20}
+
+const objetoClonado = {...pessoa1}
+console.log(objetoClonado)
+console.log(pessoa1)
