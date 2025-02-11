de78cc57647f0dcb74cf72838656b65dab77ba1c
Arthursant7s830 <135383243+Arthursant7s830@users.noreply.github.com>
2025-02-11 17:40:56 -0300

    js avancado estudos

---
+++ js-avancado/template-string/index.js
@@ -0,0 +1,14 @@
+// const imprimirInformacoespessoas = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro Nome: " + primeiroNome + "Último Nome: " + ultimoNome + "idade: " + idade;
+// }
+// console.log(imprimirInformacoespessoas("Arthur ", "Lima ", 20))
+
+
+const imprimirInformacoespessoas = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro nome: ${primeiroNome}
+    Último nome: ${ultimoNome}
+    Idade: ${idade}` ;
+
+}
+console.log(`${imprimirInformacoespessoas("Arthur", "Lima", 20)}`)
+// console.log(imprimirInformacoespessoas("Arthur ", "Lima ", 20))
