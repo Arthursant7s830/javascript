451ac67f8a907ab50ddc1de8a3a8f3806c315dec
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-05-21 08:43:24 -0300

    exercicio while 01

---
+++ exercícios/javascript intermediario/repeticoes/while001/while.js/while.js
@@ -0,0 +1,11 @@
+var contador = 3
+
+// while (contador <= 6) {
+//     console.log(`Passo ${contador}`)
+//     contador++
+// }
+
+do  {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
