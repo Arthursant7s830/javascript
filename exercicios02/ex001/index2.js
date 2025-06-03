9efbcf9f23d881700864caac371e8638c81a00c0
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-06-03 18:37:29 -0300

    .

--- exercicios02/ex001/index2.js
+++ exercicios02/ex001/index2.js
@@ -1,5 +1,26 @@
-function soma(a, b) {
-    return a + b
-}
+// function soma(a, b) {
+//     return a + b
+// }
+// console.log(soma(3, 5))
 
-console.log(soma(3, 5))
+
+
+
+
+// for ( let i = 0; i < 21; i++ ) {
+//     console.log(i)
+// }
+
+
+
+
+// let tabuada = 7
+// for(let i = 1; i < 11; i++) {
+//     console.log(`${tabuada} x ${i} = ${ tabuada * i}`)
+// }
+
+function verificador (n) {  
+    if (n > 0) {
+        return 'Positivo'
+    } else
+}
