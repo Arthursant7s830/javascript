c3f15b1876df9bf48688edf0bda21e093fed6aa0
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-06-11 17:34:50 -0300

    up

--- exercicios02/ex002/index.js
+++ exercicios02/ex002/index.js
@@ -1,5 +1,28 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let resposta = document.querySelector('div#resposta')
+let valores = []
 
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100)
+        return true
+} else {
+    return false
+}
 
-function adicionar () {
+function inLista() {
+    if (L.indexOf(Number(n)) ! = -1) {
+        return true
+    } else {
+        return false
+    }
 
 }
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value))
+
+}   else {
+    alert("Valor inválido ou já selecionado.")
+
+} 
