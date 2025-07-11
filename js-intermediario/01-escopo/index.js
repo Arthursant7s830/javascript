39833b41d99c750b399e43b9115b6d6c96552b32
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-11 12:05:23 -0300

    escopos

---
+++ js-intermediario/01-escopo/index.js
@@ -0,0 +1,39 @@
+// if(true) {
+//     const mensagem = 'Olá'
+//     console.log(mensagem)
+// }
+
+//console.log(mensagem)
+
+// {
+//   var mensagem = 'Olá'
+//          console.log(mensagem)
+// }
+// console.log(mensagem)
+
+// function falar () {
+//     var mensagem = 'Olá, mundo!'
+//     console.log(mensagem)
+// }
+
+// falar()
+// console.log(mensagem)
+let numero = 123
+
+function falar () {
+    var mensagem = 'Olá, mundo!'
+    console.log(mensagem)
+    function falarOutraCoisa() {
+        console.log(numero)
+
+    }
+
+    falarOutraCoisa()
+    console.log(falarOutraCoisa)
+}
+
+falar()
+// console.log(mensagem)
+// console.log(falarOutraCoisa)
+
+console.log(url)
