419a66276038b2e87b7319a691b1795b54c3d597
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-14 10:57:17 -0300

    Funções anônimas

---
+++ js-intermediario/03-funcoes-anonima/index.js
@@ -0,0 +1,12 @@
+// function somar (numero1, numero2) {
+//     return numero1 + numero2
+// }
+
+// let resultadoSoma = somar(1, 9)
+// console.log(resultadoSoma)
+
+let resultadoSoma = function (numero1, numero2) {
+    return numero1 + numero2
+}
+
+console.log(resultadoSoma(12, 9))
