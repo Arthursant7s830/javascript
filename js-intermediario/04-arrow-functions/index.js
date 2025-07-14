2f4dee04175a1573ac332b1716c2a3fd33fc42ff
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-14 11:10:15 -0300

    arrow functions

---
+++ js-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,9 @@
+
+let resultadoSoma = (numero1, numero2) => numero1 + numero2
+console.log(resultadoSoma(12, 9))
+
+// const incentivarQuester = nomeQuester => {
+//     console.log('Parabéns, ' +nomeQuester+ '! Você chegou ao destino!')
+// }
+
+// incentivarQuester('Arthur')
