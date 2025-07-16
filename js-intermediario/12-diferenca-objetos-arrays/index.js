51e68f883cd849439c63622ebdf7767d2ee7802c
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-16 14:37:22 -0300

    diferença entre objetos e arrays

---
+++ js-intermediario/12-diferenca-objetos-arrays/index.js
@@ -0,0 +1,18 @@
+let frutas = ['banana', 'uva']
+
+let videogame = {
+    nome: 'Xbox',
+    valor: 3000,
+    estoque: true
+}
+
+let videogame2 = {
+    nome: 'Playstation',
+    valor: 5000,
+    estoque: false
+}
+
+
+let videoGames = [videogame, videogame2]
+// console.log(videoGames)
+console.log(videoGames[1].nome)
