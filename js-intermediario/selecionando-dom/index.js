9705ea45cc7b4359915943516347928428ca2634
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-21 14:53:07 -0300

    selecionando dom

---
+++ js-intermediario/selecionando-dom/index.js
@@ -0,0 +1,15 @@
+// let p = document.getElementById('paragrafo1') 
+
+// console.log(p)
+
+// let inputClasse = document.getElementsByClassName('tag-input')
+
+// console.log(inputClasse)
+
+// let inputTag = document.getElementsByTagName( 'h1')
+
+// console.log(inputTag)
+
+let inputName = document.querySelectorAll("input[name='email]")
+
+console.log(inputName)
