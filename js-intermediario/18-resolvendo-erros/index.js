a6afc998946fce6e28e942cf041780bcd01895dc
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-07-29 16:49:02 -0300

    resolvendo erros js

---
+++ js-intermediario/18-resolvendo-erros/index.js
@@ -0,0 +1,13 @@
+// let varBoleto = 100;
+
+// console.log(varBolet)
+
+let inputTelefone = document.getElementById('telefone')
+
+console.log(inputTelefone)
+
+inputTelefone.disabled = true;
+
+function habilitar () {
+    
+}
