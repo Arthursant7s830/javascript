7e09a6569897b5e372ea348ad9092ee9186ddc5b
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-08-04 13:19:07 -0300

    conclusão

--- js-intermediario/exercicios/exercicio-01/src/slider.js
+++ js-intermediario/exercicios/exercicio-01/src/slider.js
@@ -1,24 +1,57 @@
-const imagens =document.querySelectorAll(".slide")
+const imagens = document.querySelectorAll(".slide")
 const setaVoltar = document.getElementById("seta-voltar")
 const setaAvancar = document.getElementById("seta-avancar")
 
 let imagemAtual = 0
 
 setaAvancar.addEventListener("click", function () {
-if (imagemAtual === imagens.length - 1) {
-    return
-}
+    if (imagemAtual === imagens.length - 1) {
+        return
+    }
 
-esconderImagemAberta()
+    esconderImagemAberta()
 
-imagemAtual++ 
+    imagemAtual++
 
-imagens[imagemAtual].classList.add("mostrar")
+    imagens[imagemAtual].classList.add("mostrar")
+
+    mostrarOuEsconderSetas ()
 }
 )
 
-function esconderImagemAberta(){
+setaVoltar.addEventListener("click", function () {
+    if (imagemAtual === 0) {
+        return
+    }
+
+    esconderImagemAberta()
+
+    imagemAtual --
+
+    imagens[imagemAtual].classList.add("mostrar")
+
+    mostrarOuEsconderSetas ()
+}
+)
+
+function esconderImagemAberta() {
     const imagemAberta = document.querySelector(".mostrar")
     imagemAberta.classList.remove("mostrar")
-     
+
+}
+
+function mostrarOuEsconderSetas () {
+    const naoEhAPrimeiraImagem = imagemAtual !== 0
+    if (naoEhAPrimeiraImagem) {
+        setaVoltar.classList.remove("opacidade")
+    } else {
+        setaVoltar.classList.add("opacidade")
+    }
+
+    const chegouUltimaImagem = imagemAtual !== 0 & imagemAtual === imagens.length - 1
+    if (chegouUltimaImagem) {
+        setaAvancar.classList.add("opacidade")
+    } else {
+        setaAvancar.classList.remove("opacidade")
+    }
 }
