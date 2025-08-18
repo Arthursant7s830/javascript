74fe36d149b6890feaa5b9da15173c940efc1a93
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-08-18 15:12:07 -0300

    interpolação de strings

---
+++ js-avancado/04-interpolando-strings/index.js
@@ -0,0 +1,13 @@
+// const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro nome " + primeiroNome + " Ultimo nome: "  + ultimoNome + " Idade " + idade;   
+// }
+
+// console.log(imprimirInformacoesPessoa("Arthur", "Lima", 20))]
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro nome: ${primeiroNome}, Ultimo nome: ${ultimoNome}, Idade: ${idade}`
+}
+
+// console.log(imprimirInformacoesPessoa("Arthur", "Lima", 20))
+
+console.log(`${imprimirInformacoesPessoa("Arthur", "Lima", 20)}`)
