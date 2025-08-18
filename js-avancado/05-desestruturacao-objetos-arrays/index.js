20e393424848ce98a66d4ce04c07d1d187292103
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-08-18 17:38:02 -0300

    up
    desestruturação de objetos e arrays

---
+++ js-avancado/05-desestruturacao-objetos-arrays/index.js
@@ -0,0 +1,12 @@
+let pessoa = {nome: 'Arthur', sobrenome:'Lima', idade: 20}
+// let nome = pessoa.nome
+// let idade = pessoa.idade
+
+// console.log(nome)
+// console.log(idade)
+
+let {nome: nome, idade: idade, sobrenome: sobrenome} = pessoa
+
+console.log(nome) 
+console.log(idade)
+console.log(idade)
