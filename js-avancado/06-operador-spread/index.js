82640f9680d6dc4ea3a1a5e46fb457460b05993c
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-08-20 12:04:10 -0300

    Spread

---
+++ js-avancado/06-operador-spread/index.js
@@ -0,0 +1,21 @@
+//  const pessoas = ['Arthur', 'Fulano', 'Ciclano']
+//  console.log(...pessoas)
+
+//CONCATENAR DOIS ARRAYS
+
+// let pessoas1 = ['Arthur', 'Ricardo', 'Rafael']
+// let pessoas2 = ['Fulano', 'João', 'Luiz']
+
+// pessoas1 = pessoas1.concat(pessoas2 ) 
+
+// pessoas1 = [...pessoas1, ...pessoas2]
+// console.log(pessoas1)
+
+// CLONAR OBJETO
+
+const pessoa1 = {nome: 'Arthur', idade: 20}
+const pessoa2 = {nome: 'Fulano', idade: 33}
+
+const objetoClonado = {...pessoa1}
+console.log(objetoClonado)
+
