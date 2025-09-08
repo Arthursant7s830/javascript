c8758b4ac8ab0070ac40c9275d80c9570334f8ac
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-09-08 09:18:41 -0300

    datas no js

---
+++ js-avancado/16-datas/index.js
@@ -0,0 +1,34 @@
+// const dataAtual = new Date()
+// const dia = dataAtual.getDate()
+// const mes = dataAtual.getMonth() +1
+// const ano = dataAtual.getFullYear()
+
+// console.log(dataAtual)
+// console.log(dia)
+// console.log(mes)
+// console.log(ano)
+
+
+
+//SOMANDO DIAS NAS DATAS
+// const dataAtual = new Date();
+// dataAtual.setDate(dataAtual.getDate() -7);
+// console.log(dataAtual);
+
+
+
+
+// const dataNascimento = new Date('2004-09-23');
+// const dataAtual = new Date();
+
+// //CALCULANDO A DIFERENÇA EM MILESSEGUNDOS
+// const diferencaEmMilessegundos = dataAtual - dataNascimento;
+
+// // CONVERTENDO A DIFERENÇA PARA ANOS
+// const idade = Math.floor(diferencaEmMilessegundos / (365.25 * 24 * 60 * 60 * 1000));
+// console.log(idade)
+
+// FORMAtANDO DATA LOCAL
+// const dataAtual = new Date();
+// const dataFormatadada = dataAtual.toLocaleDateString('pt-BR');
+// console.log(dataFormatadada);
